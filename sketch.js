9f2ccd38745bd96e03484daeaed4b0ffd0d16c8f
Shadow2888 <72982446+Shadow2888@users.noreply.github.com>
2021-01-19 22:36:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,14 +2,15 @@ var p1;
 var e1,e2,e3,e4,e5,e6,e7,e8,eg,ebg;
 var sq1,sq2,sq3,sq4,sq5;
 var b1;
-var pb,eb,pm,pt,pbg,exp;
+var pb1,pb2,eb,pm,pt,pbg,exp;
 var lc=0,life,life1;
 var PLAY=1;
 var END=0;
-var gamestate=PLAY;
-var home;
+var HOME;
+var TOT;
+var gamestate=HOME;
 var gamestate2=1;
-
+var akagi;
 
 
 
@@ -20,13 +21,18 @@ function preload(){
   bg=loadImage("images/bg.png");
   p1i=loadImage("images/p1.png");
 exp=loadImage("images/exp.png");
-
+ijnakagi =loadImage("images/ijn akagi.png");
   }
 
 function setup() {
   createCanvas(800,800);
-  p1 = createSprite(400, 400, 50, 50);
+
+  akagi = createSprite(400, 400, 50, 50);
+  akagi.addImage(ijnakagi);
+
+  p1 = createSprite(413, 400, 50, 50);
   p1.addImage(p1i);
+ 
  
   eg=new Group ();
   pbg=new Group ();
@@ -42,11 +48,33 @@ function draw() {
   background(bg);
  
 
- if(gamestate === PLAY){
-  enemy ();
+  if(gamestate === HOME){
 
-  p1.x=mouseX;
-  p1.y=mouseY;
+ 
+p1.scale=0.5;
+
+  akagi.velocityY=2;
+  akagi.lifetime=280;
+
+
+
+  if(akagi.y===900){
+akagi.destroy();
+    p1.scale=1;
+    gamestate=PLAY;
+
+  }
+
+   }
+
+   //if(gamestate === TOT){
+
+  // }
+   if(gamestate === PLAY){
+    enemy ();
+
+    p1.x=mouseX;
+    p1.y=mouseY;
 
 //if(gamestate===PLAY){
 //  efire();
@@ -57,17 +85,24 @@ if(keyDown("space")){
 }
 
 if(pbg.isTouching(eg)){
-  eg.destroyEach();
-  pbg.destroyEach();
+  for (var i = 0; i < eg.length; i++) {
+     if (eg.get(i).isTouching(pbg)) {
+        eg.get(i).destroy()
+        ;
+       }
+      }
+  
+ // eg.destroyEach();
+ // pbg.destroyEach();
   }
 
   if(ebg.isTouching(p1)){
     
      lc=lc+1;
-    life.width=life.width-2;
+    life.width=life.width-1;
      }
 
-     if(lc===100){
+     if(lc===400){
       gamestate=END;
       p1.lifetime=150;
         p1.addImage(exp);
@@ -115,6 +150,7 @@ function enemy () {
    e1.velocityY=3;
    eg.add (e1);
    efire();
+   e1.lifetime=280;
    e1.addImage(f4u)
    
  }
@@ -125,6 +161,7 @@ function enemy () {
     e1.velocityX=1;
     eg.add (e1);
     efire();
+    e1.lifetime=280;
     e1.addImage(f4u)
  }
  if(frameCount%30===0){ 
@@ -134,7 +171,8 @@ function enemy () {
   e1.velocityX=-1;
   eg.add (e1);
   efire();
-  e1.addImage(f4u)
+  e1.lifetime=280;
+    e1.addImage(f4u)
 }
 
 //function bomber
@@ -157,11 +195,16 @@ function bomber(){
 }
 
 function fire (){
-  pb = createSprite(p1.x, p1.y, 2, 5);
-pb.velocityY=-16;
-pb.shapeColor="yellow";
-pbg.add(pb);
-
+  pb1 = createSprite(p1.x-17, p1.y, 2, 5);
+  pb2= createSprite(p1.x+17, p1.y, 2, 5);
+pb1.velocityY=-16;
+pb1.shapeColor="yellow";
+pbg.add(pb1);
+pb1.lifetime=40;
+pb2.velocityY=-16;
+pb2.shapeColor="yellow";
+pbg.add(pb2);
+pb2.lifetime=40;
 }
 
 function efire (){
@@ -169,5 +212,5 @@ function efire (){
 eb.velocityY=16;
 eb.shapeColor="yellow";
 ebg.add(eb);
-
+eb.lifetime=80;
 }
